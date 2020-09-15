500a2a0cfce313062cbaa8333d7804bd89d44cca
dtid_4cb73ba1d0cd43af <396751927@qq.com>
2020-09-15 13:10:17 +0800

    增加数据库计划任务

--- src/cron/Service.php
+++ src/cron/Service.php
@@ -2,6 +2,7 @@
 
 namespace yzh52521\cron;
 
+use yzh52521\cron\command\MySql;
 use yzh52521\cron\command\Run;
 use yzh52521\cron\command\Schedule;
 
@@ -11,6 +12,7 @@ class Service extends \think\Service
     public function boot()
     {
         $this->commands([
+            MySql::class,
             Run::class,
             Schedule::class,
         ]);
